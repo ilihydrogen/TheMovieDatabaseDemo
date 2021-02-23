bfe22b74cf7a7037ac27ce14a6d6656a5605c4bc
iHydroGen <ilihydrogen>
2021-02-23 16:01:14 +0300

    layout background fixes

--- app/src/main/java/ru/mikhailskiy/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
